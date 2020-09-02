71f2628b29d28d8b8efbadb0d619d4e5b3d0ce49
wesinator <5124946+wesinator@users.noreply.github.com>
2020-09-02 10:45:31 -0400

    add webextension files

---
+++ README.md
@@ -0,0 +1,8 @@
+# XNfire
+Firefox extension to warn browser users when the site they visit is an IDN domain.
+
+Mozilla decided to keep IDN domain rendering in the address bar, this extension warns users
+whenever the real hostname is an IDN domain, to help identify IDN phishing.
+
+#### Download
+[![FF Version](https://img.shields.io/amo/v/xnfire.svg)](https://addons.mozilla.org/addon/xnfire/)
---
+++ webextension/idndetect.js
@@ -0,0 +1,16 @@
+function isIDN(hostname) {
+  for (part of hostname.split(".")) {
+    if (part.startsWith("xn--"))
+      return true;
+  }
+  return false;
+}
+
+var hostname = window.location.hostname;
+if (isIDN(hostname)) {
+  var msg = `Warning: the actual domain you visited is "${hostname}". This is an internationalized domain name (IDN).
+If the address bar appears to show a site you were intending to visit, you should not enter any information here, leave the page and type the site manually.`;
+
+  alert(msg);
+  console.warn(msg);
+}
